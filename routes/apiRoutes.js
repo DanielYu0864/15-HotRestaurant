36ceb3e70216d88f28a5f2cfc09cb3aa0ed15fe9
Daniel.Yu <adam741963@gmail.com>
2020-08-24 20:35:43 -0700

    servers

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,1 +1,25 @@
-const a;
+const table = require("../data/table");
+const waitlist = require("../data/waitinglist");
+
+const data = function(app) {
+
+    app.get("/api/tables", function(req, res) {
+        res.json(table);
+    });
+
+    app.get("/api/waitlist", function(req, res) {
+        res.json(table);
+    });
+
+    app.post("/api/tables", function(req,res) {
+        if (table.length < 5) {
+            table.push(req.body);
+            res.json(true);
+        }
+
+        else {
+            waitListData.push(req.body);
+            res.json(false);
+        }
+    });
+};
