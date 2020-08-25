b34efcfddec29226828184ec266061f6dcc8168b
Vasile <tronciu92@gmail.com>
2020-08-24 20:37:43 -0700

    newstuff

---
+++ data/waitinglist.js
@@ -0,0 +1,10 @@
+var waitingArray = [
+    {
+      customerName: "Daniel Yu",
+      customerEmail: "daniel.yu@example.com",
+      phoneNumber: "000-000-0000",
+      customerID: "danDude"
+    }
+  ];
+  
+  module.exports = waitingArray;
